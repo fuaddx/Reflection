d7f5cdd7c28893b40c70f460a5425bc8f6503eca
Fuad <mi848xfcl@code.edu.az>
2023-11-03 02:07:15 +0400

    reflection

---
+++ Reflection/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+
+namespace Reflection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            User user = new User(1, "Fuad");
+            Console.WriteLine("First Information");
+            user.UserInfo();
+
+            var ageField = typeof(User).GetField("Age", BindingFlags.NonPublic | BindingFlags.Static);
+            
+                ageField.SetValue(user, 20);
+            
+
+            Console.WriteLine("Changed Info");
+            user.UserInfo();
+        }
+    }
+}
---
+++ Reflection/User.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reflection
+{
+    internal class User
+    {
+
+        private int id;
+        private string name;
+        private static int Age = 10;
+
+        public User(int id, string name)
+        {
+            this.id = id;
+            this.name = name;
+        }
+
+        public void UserInfo()
+        {
+            Console.WriteLine($"ID: {id}");
+            Console.WriteLine($"Name: {name}");
+            Console.WriteLine($"Age: {Age}");
+        }
+
+        public static void ChangeAge(int age)
+        {
+            Age = age;
+        }
+
+
+    }
+}
